9b6cfc31929a9c208c4ddabee2a8d8383deaf6a3
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-08 22:31:55 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 setInterval(Tick, 1000)
+
 let window_open = false
 let TARGET_WINDOW;
 
+let run_tick = true;
+
 function Tick(){
-    if(!document.getElementById("bot_active_checkbox").checked || document.getElementById("target_url").value == "") return
+    if(!document.getElementById("bot_active_checkbox").checked || document.getElementById("target_url").value == "" || !run_tick) return
 
     if(!window_open){
         TARGET_WINDOW = window.open(document.getElementById("target_url").value, "_blank")
         window_open = true
     }
 
-    
+    let memory_usage = parseFloat(TARGET_WINDOW.document.getElementsByClassName("info_mem bottom_status")[0].children[1].children[0].innerHTML)
+    if(memory_usage < 2.0){
+        var element = TARGET_WINDOW.document.getElementsByClassName("gi gi-reload3")[0].parentElement
+        if(document.createEvent)
+        {
+        element.dispatchEvent(new Event('mousedown'));
+        }
+        else{
+        // Internet Explorer (I think)
+        element.fireEvent("onmousedown", event); 
+        }
+
+        run_tick = false
+        setTimeout(() => {
+            run_tick = true;
+        }, 15000);
+    }
 }
 
 function CheckConnectionState(){
 
 }
+
+//TARGET_WINDOW.document.getElementsByClassName("info_mem bottom_status")[0].children[1].children[0].innerHTML
