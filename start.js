adc755c185fdb63307589efe2370ed03d77b991e
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-08 22:32:20 +0600

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,17 @@
+let html_url = "https://shikto-tech.github.io/GKAB/index.html"
 
+fetch(html_url)
+  .then(response => response.text())
+  .then(data => add_html_and_script(data));
+
+function add_html_and_script(dt){
+    document.body.innerHTML = dt
+    add_script("https://shikto-tech.github.io/GKAB/discord_bot.js")
+    add_script("https://shikto-tech.github.io/GKAB/main.js")
+}
+
+function add_script(url){
+    let nsc = document.createElement("script")
+    nsc.src = url
+    document.body.appendChild(nsc)
+}
