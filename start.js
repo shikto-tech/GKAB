43d5099f310d3447a2447b8c4952417b30edfbf2
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-09 15:17:39 +0600

    Update start.js

--- start.js
+++ start.js
@@ -1,17 +1,23 @@
-let html_url = "https://shikto-tech.github.io/GKAB/index.html"
+console.log("LOCATION: " + window.location.href)
+if(window.location.href == "https://ide-run.goorm.io/workspace"){
+  console.log("started GKAB")
+  document.body.innerHTML = "TEST"
+  let html_url = "https://shikto-tech.github.io/GKAB/index.html"
+  //add_script("https://shikto-tech.github.io/GKAB/start.js")
 
-fetch(html_url)
-  .then(response => response.text())
-  .then(data => add_html_and_script(data));
+  fetch(html_url)
+    .then(response => response.text())
+    .then(data => add_html_and_script(data));
 
-function add_html_and_script(dt){
-    document.body.innerHTML = dt
-    add_script("https://shikto-tech.github.io/GKAB/discord_bot.js")
-    add_script("https://shikto-tech.github.io/GKAB/main.js")
-}
+  function add_html_and_script(dt){
+      document.body.innerHTML = dt
+      add_script("https://shikto-tech.github.io/GKAB/discord_bot.js")
+      add_script("https://shikto-tech.github.io/GKAB/main.js")
+  }
 
-function add_script(url){
-    let nsc = document.createElement("script")
-    nsc.src = url
-    document.body.appendChild(nsc)
+  function add_script(url){
+      let nsc = document.createElement("script")
+      nsc.src = url
+      document.body.appendChild(nsc)
+  }
 }
