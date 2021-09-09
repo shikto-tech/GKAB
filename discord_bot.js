6ed6648318d5b73abb3f852286d2ff071f081f13
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-09 21:33:34 +0600

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -3,9 +3,10 @@ let channelID = "884864014176370772"
 let bot_token = "ODg0ODYzMzU1ODg0NTM5OTM0.YTerOA" + "" + ".ajvn-D7_YAGYeo7jn88wjP6YWBk"
 
 let url = "https://discordapp.com/api/channels/" + channelID + "/messages"
-let headers = { "Authorization":"Bot " + bot_token, "User-Agent":"myBotThing (http://some.url, v0.1)", "Content-Type":"application/json", }
+let headers = { "Authorization":"Bot " + bot_token, "User-Agent":"Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)", "Content-Type":"application/json", }
 
 function DISCORD_SEND(messsage) {
+    LOG("DISCORD-- sending a message: " + messsage)
     messsage = messsage || ""
     const request = new XMLHttpRequest();
     request.open("POST", url);
